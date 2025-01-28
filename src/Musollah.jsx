f85feffe20824de9d74598090f69b9205f1257f8
Sorarat Urairaksa <sorarat.urairaksa@gmail.com>
2025-01-28 16:36:07 +0700

    adjust musollah pics on medium screen or larger

--- src/Musollah.jsx
+++ src/Musollah.jsx
@@ -7,14 +7,14 @@ const Musollah = () => {
         <div className="p-5">
           <h2 className="text-3xl font-semibold m-4">MUSOLLAH</h2>
           <div className="md:flex md:items-start md:p-3 ">
-            <div className="text-left font-light mb-10 mt-5 md:w-1/2 ">
+            <div className="text-left font-light mb-10 mt-5 md:w-1/2 md:leading-8 ">
               <p>Our Musollah is located at Staircase 24, Block A, right next to A.1.17. 
                   It’s open to all, and prayer attires are provided for anyone who needs them. 
                   Feel free to use the slippers available to take wudhu comfortably. 
                   Don’t forget to check the bulletin board for the latest MCMS events and updates!
               </p>
             </div>
-            <div className="flex flex-col items-center mx-auto gap-y-2 w-36 md:flex-row md:mr-8 md:order-first">
+            <div className="flex flex-col items-center mx-auto gap-y-2 w-36 md:flex-row md:mr-8 md:w-1/2 md:gap-x-3 md:order-first">
               <div>
                 <img
                   className="h-auto max-w-full rounded-md"
