5de94e4decccc06b85db609301933a33de8bd566
Sorarat Urairaksa <sorarat.urairaksa@gmail.com>
2025-02-07 17:23:32 +0700

    fix photo text alignment in events page

--- src/Events.jsx
+++ src/Events.jsx
@@ -18,68 +18,77 @@ const Events = () => {
       <div className="bg-lightYellow p-6 md:p-16">
         <div>
           <h4 className="text-2xl font-medium m-4 md:text-left">Orientation</h4>
-          <div className="flex flex-col md:flex-row ">
-            <p className="text-left font-light mt-3 leading-2 md:leading-7">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Proin eu placerat ex, a dignissim orci. In imperdiet nibh eu luctus viverra. 
-            Sed elementum quam consectetur neque congue suscipit. Cras ultricies dictum lobortis. 
-            </p>
+          <div className="flex flex-col md:flex-row md:items-start"> 
             <img
-              className="h-auto max-w-full rounded-lg mt-4 sm:max-h-[400px] sm:max-w-[90%] md:max-h-[600px] md:max-w-[55%] lg:max-h-[600px] lg:max-w-[45%] mx-auto md:m-4 md:order-first"
+              className="h-auto max-w-full rounded-lg mt-4 sm:max-h-[400px] sm:max-w-[90%] md:max-h-[600px] md:max-w-[55%] lg:max-w-[45%] mx-auto md:m-4 md:order-first"
               src="src/images/img3.jpg"
               alt="image 3"
             />
+            <div className="md:ml-4"> 
+              <p className="text-left font-light mt-3 leading-2 md:leading-7">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Proin eu placerat ex, a dignissim orci. In imperdiet nibh eu luctus viverra.
+                Sed elementum quam consectetur neque congue suscipit. Cras ultricies dictum lobortis.
+              </p>
+            </div>
           </div>
         </div>
 
         <div>
-          <h4 className="text-2xl font-medium m-4 mt-10 md:text-left">Raya</h4>
-          <div className="flex flex-col md:flex-row ">
-            <p className="text-left font-light mt-3 leading-2 md:leading-7">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Proin eu placerat ex, a dignissim orci. In imperdiet nibh eu luctus viverra. 
-            Sed elementum quam consectetur neque congue suscipit. Cras ultricies dictum lobortis. 
-            </p>
+          <h4 className="text-2xl font-medium m-4 md:text-left">Raya</h4>
+          <div className="flex flex-col md:flex-row md:items-start"> 
             <img
-              className="h-auto max-w-full rounded-lg mt-4 sm:max-h-[400px] sm:max-w-[90%] md:max-h-[600px] md:max-w-[55%] lg:max-h-[600px] lg:max-w-[45%] mx-auto md:m-4 md:order-first"
+              className="h-auto max-w-full rounded-lg mt-4 sm:max-h-[400px] sm:max-w-[90%] md:max-h-[600px] md:max-w-[55%] lg:max-w-[45%] mx-auto md:m-4 md:order-first"
               src="src/images/img3.jpg"
               alt="image 3"
             />
-          </div>
-        </div>
-
-
-        <div>
-          <h4 className="text-2xl font-medium m-4 mt-10 md:text-left">Iftarawih</h4>
-          <div className="flex flex-col md:flex-row ">
-            <p className="text-left font-light mt-3 leading-2 md:leading-7">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Proin eu placerat ex, a dignissim orci. In imperdiet nibh eu luctus viverra. 
-            Sed elementum quam consectetur neque congue suscipit. Cras ultricies dictum lobortis. 
-            </p>
-            <img
-              className="h-auto max-w-full rounded-lg mt-4 sm:max-h-[400px] sm:max-w-[90%] md:max-h-[600px] md:max-w-[55%] lg:max-h-[600px] lg:max-w-[45%] mx-auto md:m-4 md:order-first"
-              src="src/images/img3.jpg"
-              alt="image 3"
-            />
+            <div className="md:ml-4"> 
+              <p className="text-left font-light mt-3 leading-2 md:leading-7">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Proin eu placerat ex, a dignissim orci. In imperdiet nibh eu luctus viverra.
+                Sed elementum quam consectetur neque congue suscipit. Cras ultricies dictum lobortis.
+              </p>
+            </div>
           </div>
         </div>
 
         <div>
-          <h4 className="text-2xl font-medium m-4 mt-10 md:text-left">Sports Day</h4>
-          <div className="flex flex-col md:flex-row ">
-            <p className="text-left font-light mt-3 leading-2 md:leading-7">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Proin eu placerat ex, a dignissim orci. In imperdiet nibh eu luctus viverra. 
-            Sed elementum quam consectetur neque congue suscipit. Cras ultricies dictum lobortis. 
-            </p>
+          <h4 className="text-2xl font-medium m-4 md:text-left">Iftarawih</h4>
+          <div className="flex flex-col md:flex-row md:items-start"> 
             <img
-              className="h-auto max-w-full rounded-lg mt-4 sm:max-h-[400px] sm:max-w-[90%] md:max-h-[600px] md:max-w-[55%] lg:max-h-[600px] lg:max-w-[45%] mx-auto md:m-4 md:order-first"
+              className="h-auto max-w-full rounded-lg mt-4 sm:max-h-[400px] sm:max-w-[90%] md:max-h-[600px] md:max-w-[55%] lg:max-w-[45%] mx-auto md:m-4 md:order-first"
               src="src/images/img3.jpg"
               alt="image 3"
             />
+            <div className="md:ml-4"> 
+              <p className="text-left font-light mt-3 leading-2 md:leading-7">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Proin eu placerat ex, a dignissim orci. In imperdiet nibh eu luctus viverra.
+                Sed elementum quam consectetur neque congue suscipit. Cras ultricies dictum lobortis.
+              </p>
+            </div>
           </div>
         </div>
+
+        <div>
+          <h4 className="text-2xl font-medium m-4 md:text-left">Sports Day</h4>
+          <div className="flex flex-col md:flex-row md:items-start"> 
+            <img
+              className="h-auto max-w-full rounded-lg mt-4 sm:max-h-[400px] sm:max-w-[90%] md:max-h-[600px] md:max-w-[55%] lg:max-w-[45%] mx-auto md:m-4 md:order-first"
+              src="src/images/img3.jpg"
+              alt="image 3"
+            />
+            <div className="md:ml-4"> 
+              <p className="text-left font-light mt-3 leading-2 md:leading-7">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Proin eu placerat ex, a dignissim orci. In imperdiet nibh eu luctus viverra.
+                Sed elementum quam consectetur neque congue suscipit. Cras ultricies dictum lobortis.
+              </p>
+            </div>
+          </div>
+        </div>
+
+       
 
         <div>
 
