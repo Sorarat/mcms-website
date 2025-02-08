a8c97c4111769f4ad823973c1f13b547206f95f8
Sorarat Urairaksa <sorarat.urairaksa@gmail.com>
2025-02-08 17:33:44 +0700

    vertically center the photo description

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -44,7 +44,7 @@ const Gallery = () => {
   return (
     <div className="container mx-auto p-4">
       {/* Image Gallery */}
-      <div className="flex flex-col md:flex-row md:gap-8">
+      <div className="flex flex-col md:flex-row md:gap-8 md:items-center">
         <div className="flex-1">
           <ImageGallery
             items={images}
@@ -55,7 +55,7 @@ const Gallery = () => {
           />
         </div>
          {/* Independent Description */}
-        <div className="mt-4 md:mt-0 md:flex-1">
+        <div className="mt-4 md:mt-0 md:flex-1 mb-28">
           <GalleryDescription title={currentTitle} description={currentDescription} />
         </div>
       </div>
